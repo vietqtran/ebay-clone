f59c37b4701d625124c6678360419912d1adf0a6
viettq3 <viettq3@rikkeisoft.com>
2024-09-05 17:32:28 +0700

    add supabase

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,11 +1,3 @@
-import { createClient } from '@/utils/supabase/server'
-import { cookies } from 'next/headers'
-
-export default async function Page() {
-   const cookieStore = cookies()
-   const supabase = createClient(cookieStore)
-
-   const { data: todos } = await supabase.from('todos').select()
-   console.log(todos)
-   return <ul>{todos?.map((todo: any) => <li key={todo.id}>{todo.created_at}</li>)}</ul>
+export default function Register() {
+   return <h1>HomePage</h1>
 }
