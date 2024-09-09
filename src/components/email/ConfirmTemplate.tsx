6dc091773fc78f444148fc8a25820c9579c938e4
viettq3 <viettq3@rikkeisoft.com>
2024-09-09 12:03:06 +0700

    feat: send mail

---
+++ src/components/email/ConfirmTemplate.tsx
@@ -0,0 +1,13 @@
+// components/email/ConfirmTemplate.tsx
+import React from 'react'
+
+const ConfirmTemplate: React.FC = () => {
+   return (
+      <div>
+         <h1>Confirmation Email</h1>
+         <p>Your account has been confirmed!</p>
+      </div>
+   )
+}
+
+export default ConfirmTemplate
